0c6fa4577a9243611ee1731aafc707c66a66d1f2
dreamf_pro <dreamf_pro@qq.com>
2021-05-14 15:58:17 +0800

    first commit

---
+++ src/main/java/com/njupt/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.njupt.git;
+
+/**
+ * @Auther ChenFei
+ * @Date Create On 2021/5/14 15:56
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
